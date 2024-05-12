6dfe973f75bfffb88f50360de0297c25491dc58f
Usaid Asif <usaidasif12@gmail.com>
2024-05-12 15:41:23 +0500

    set up the project

---
+++ lib/pedro_spinner.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PedroSpinner extends StatefulWidget {
+  const PedroSpinner({super.key});
+
+  @override
+  State<PedroSpinner> createState() => _PedroSpinnerState();
+}
+
+class _PedroSpinnerState extends State<PedroSpinner> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
